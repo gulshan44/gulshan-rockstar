62ccdff9a26966fe0df7a46c44af4340be78522f
Gulshan Verma <gulshanrock4477@gmail.com>
2024-12-01 19:35:04 +0530

    Your commit message

--- src/component/ChoosePic.js
+++ src/component/ChoosePic.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ChoosePic = () => {
+
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    const images = [
+        "/img1.jpg",
+        "/img1.jpg",
+        "/img1.jpg",
+        "/img1.jpg",
+        "/img1.jpg",
+        "/img1.jpg",
+    ];
+
     return (
         <>
             <div className='container py-5 text-center categories'>
@@ -10,28 +22,32 @@ const ChoosePic = () => {
 
                 <div className='row pt-5 box'>
 
-                    <div className='col-6 col-md-4 categories_box'>
-                        <img src='/img1.jpg' alt='...' />
-                    </div>
-                    <div className='col-6 col-md-4 categories_box'>
-                        <img src='/img1.jpg' alt='...' />
-                    </div>
-                    <div className='col-6 col-md-4 categories_box'>
-                        <img src='/img1.jpg' alt='...' />
-                    </div>
+                    {images.map((image, index) => (
+                        <div className='col-6 col-md-4 categories_box'>
+                            <img src='/img1.jpg' alt='...' />
+                        </div>
+                    ))}
 
-                    <div className='col-6 col-md-4 categories_box'>
-                        <img src='/img1.jpg' alt='...' />
-                    </div>
-                    <div className='col-6 col-md-4 categories_box'>
-                        <img src='/img1.jpg' alt='...' />
-                    </div>
-                    <div className='col-6 col-md-4 categories_box'>
-                        <img src='/img1.jpg' alt='...' />
-                    </div>
-                  
                 </div>
             </div>
+
+            {selectedImage && (
+                <div className="modal fade show"
+                    style={{ display: "block", backgroundColor: "rgba(0, 0, 0, 0.8)" }}
+                    tabIndex="-1"
+                >
+                    <div className="modal-dialog modal-dialog-centered">
+                        <div className="modal-content">
+                            <div className="modal-body">
+                                <img src={selectedImage} alt="Selected" className="img-fluid" />
+                            </div>
+                            <button type="button" className="btn-close"
+                                style={{ position: "absolute", top: "10px", right: "10px", background: "white", borderRadius: "50%",}}
+                                onClick={() => setSelectedImage(null)}></button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </>
     )
 }
