715ee5a464bd7c954bfbf88ad313434fad0da2bc
Gulshan Verma <gulshanrock4477@gmail.com>
2024-12-01 10:38:40 +0530

    Your commit message

--- src/component/Homepage.js
+++ src/component/Homepage.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Homepage = () => {
+
     return (
         <div>
 
@@ -8,16 +10,18 @@ const Homepage = () => {
                 <div className="carousel-inner">
                     <div className="carousel-item active">
                         <img src="/slider1.jpg" className="d-block w-100" alt="..." />
+
+                        <div className='heading'>
+                            <p>Hello,</p>
+                            <h1>Welcome To Rockstar Gallery</h1>
+                            <p>We are glad to have you here. Explore the amazing collection of images.</p>
+                            <Link to='/' className='btn btn-outline-info'>Get Started</Link>
+                        </div>
+
                     </div>
-                    <div className="carousel-item">
-                        <img src="/slider1.jpg" className="d-block w-100" alt="..." />
-                    </div>
-                    <div className="carousel-item">
-                        <img src="/slider1.jpg" className="d-block w-100" alt="..." />
-                    </div>
+                
                 </div>
             </div>
-
 
         </div>
     )
