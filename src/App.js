db01309b897483c28d347c1fe18e7946ab2802ee
Gulshan Verma <gulshanrock4477@gmail.com>
2024-12-01 19:15:15 +0530

    Your commit message

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Gallery from './component/Gallery'
 import Homepage from './component/Homepage'
 import Profile from './component/Profile'
+import ChoosePic from './component/ChoosePic'
 
 const App = () => {
   return (
@@ -13,9 +14,11 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path='/' element={<Homepage />} />
+          {/* <Route path='/' element={<ChoosePic />} /> */}
           <Route path='gallery' element={<Gallery />} />
           <Route path='profile' element={<Profile />} />
         </Routes>
+        <ChoosePic/>
       </Router>
 
     </div>
---
+++ src/component/ChoosePic.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const ChoosePic = () => {
+    return (
+        <>
+            <div className='container py-5 text-center categories'>
+
+                <h2>CHOOSE FROM CATEGORIES</h2>
+                <p>"Unlock a gallery of moments frozen in time. Our categories bring together the best of creativity, beauty, and imagination to help you find exactly what resonates with your heart."</p>
+
+                <div className='row pt-5 box'>
+
+                    <div className='col-6 col-md-4 categories_box'>
+                        <img src='/img1.jpg' alt='...' />
+                    </div>
+                    <div className='col-6 col-md-4 categories_box'>
+                        <img src='/img1.jpg' alt='...' />
+                    </div>
+                    <div className='col-6 col-md-4 categories_box'>
+                        <img src='/img1.jpg' alt='...' />
+                    </div>
+
+                    <div className='col-6 col-md-4 categories_box'>
+                        <img src='/img1.jpg' alt='...' />
+                    </div>
+                    <div className='col-6 col-md-4 categories_box'>
+                        <img src='/img1.jpg' alt='...' />
+                    </div>
+                    <div className='col-6 col-md-4 categories_box'>
+                        <img src='/img1.jpg' alt='...' />
+                    </div>
+                  
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ChoosePic
