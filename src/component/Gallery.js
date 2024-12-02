f996c6e360139de0c9014b739c8ab131fa28a09b
Gulshan Verma <gulshanrock4477@gmail.com>
2024-12-02 11:27:42 +0530

    Your commit message

--- src/component/Gallery.js
+++ src/component/Gallery.js
@@ -1,12 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Gallery = () => {
-  return (
-    <div className='container'>
-        <h1>Gallery Page</h1>
-        <p>This is Gallery page</p>
-    </div>
-  )
+
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    const images = [
+        "/img1.jpg",
+        "/img2.jpg",
+        "/img3.jpg",
+        "/img1.jpg",
+        "/img1.jpg",
+        "/img1.jpg",
+        "/img1.jpg",
+        "/img2.jpg",
+        "/img3.jpg",
+        "/img1.jpg",
+        "/img1.jpg",
+        "/img1.jpg",
+
+    ];
+
+    return (
+        <div className='container-sm'>
+            <h1>Gallery Page</h1>
+            <p>This is Gallery page</p>
+            <div className='row'>
+
+                {images.map((image, index) => (
+
+                    <div key={index} className='col-md-2 col-3 p-0 gallery_img'>
+                        <img src={image} alt={`${index + 1}`} onClick={() => setSelectedImage(image)} className='img-fluid'/>
+                    </div>
+                ))}
+
+            </div>
+        </div>
+    )
 }
 
 export default Gallery
