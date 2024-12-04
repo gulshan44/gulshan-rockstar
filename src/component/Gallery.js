9d78b659a174f9a96693d1b1315e9fde4b86ddbb
Gulshan Verma <gulshanrock4477@gmail.com>
2024-12-04 12:26:47 +0530

    Your commit message

--- src/component/Gallery.js
+++ src/component/Gallery.js
@@ -7,19 +7,19 @@ const Gallery = () => {
 
     const images = [
         "/img1.jpg",
-        "/img2.jpg",
-        "/img3.jpg",
-        "/img4.png",
-        "/img5.jpg",
-        "/img6.jpg",
-        "/img7.jpg",
-        "/img8.jpg",
-        "/img9.jpg",
-        "/img10.jpg",
-        "/img11.jpg",
-        "/img12.jpg",
-        "/img13.jpg",
-        "/img14.jpg",
+        // "/img2.jpg",
+        // "/img3.jpg",
+        // "/img4.png",
+        // "/img5.jpg",
+        // "/img6.jpg",
+        // "/img7.jpg",
+        // "/img8.jpg",
+        // "/img9.jpg",
+        // "/img10.jpg",
+        // "/img11.jpg",
+        // "/img12.jpg",
+        // "/img13.jpg",
+        // "/img14.jpg",
     ];
 
     return (
